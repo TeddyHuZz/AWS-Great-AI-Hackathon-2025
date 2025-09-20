b259018acd3395056f106071310ec5e37dc60c71
TeddyHuZz <feigaming04XP@gmail.com>
2025-09-21 00:53:46 +0800

    fixed dashboard not running

---
+++ src/Dashboard.js
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import "./Dashboard.css";
+
+function Dashboard() {
+    return (
+        <div className="Dashboard">
+            <Header />
+        </div>
+        
+    )
+}
+
+export default Dashboard;
